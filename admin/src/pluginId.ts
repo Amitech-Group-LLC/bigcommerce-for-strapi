ac78cb31e3ea8388765a0da338e818f3628c12a2
Alex Simonok <sashasimonok@gmail.com>
2022-12-12 16:54:40 +0300

    Fix models according to plugin name

--- admin/src/pluginId.ts
+++ admin/src/pluginId.ts
@@ -1,5 +1,3 @@
-import pluginPkg from '../../package.json';
-
-const pluginId = pluginPkg.name.replace(/^(@[^-,.][\w,-]+\/|strapi-)plugin-/i, '');
+const pluginId = 'bigcommerce'
 
 export default pluginId;
